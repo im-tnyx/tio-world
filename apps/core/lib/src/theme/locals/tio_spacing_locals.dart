e1e33f878f3057fac30523745f33dcdac939fb5a
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:53:46 +0530

    feat: fill spacing locals

--- apps/core/lib/src/theme/locals/tio_spacing_locals.dart
+++ apps/core/lib/src/theme/locals/tio_spacing_locals.dart
@@ -1,1 +1,10 @@
-// Spacing local accessors.
+import '../tokens/foundation/tio_spacing.dart';
+
+class TioSpacingLocals {
+  const TioSpacingLocals._();
+
+  static const small = TioSpacing.small;
+  static const medium = TioSpacing.medium;
+  static const large = TioSpacing.large;
+  static const extraLarge = TioSpacing.extraLarge;
+}
