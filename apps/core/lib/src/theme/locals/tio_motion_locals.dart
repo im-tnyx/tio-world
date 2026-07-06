b5c1fb925a9f279b5d995d100d95064853f5a8d5
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:54:24 +0530

    feat: fill motion locals

--- apps/core/lib/src/theme/locals/tio_motion_locals.dart
+++ apps/core/lib/src/theme/locals/tio_motion_locals.dart
@@ -1,1 +1,9 @@
-// Motion local accessors.
+import '../tokens/effects/tio_motion.dart';
+
+class TioMotionLocals {
+  const TioMotionLocals._();
+
+  static const fastMs = TioMotion.fastMs;
+  static const normalMs = TioMotion.normalMs;
+  static const slowMs = TioMotion.slowMs;
+}
