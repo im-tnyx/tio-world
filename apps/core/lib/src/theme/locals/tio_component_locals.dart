663c073c0de3627768669e96ee78900d5a310ae4
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:54:59 +0530

    feat: fill component locals

--- apps/core/lib/src/theme/locals/tio_component_locals.dart
+++ apps/core/lib/src/theme/locals/tio_component_locals.dart
@@ -1,1 +1,15 @@
-// Component token local accessors.
+import '../tokens/components/tio_button_tokens.dart';
+import '../tokens/components/tio_card_tokens.dart';
+import '../tokens/components/tio_input_tokens.dart';
+import '../tokens/components/tio_navigation_tokens.dart';
+import '../tokens/components/tio_sheet_tokens.dart';
+
+class TioComponentLocals {
+  const TioComponentLocals._();
+
+  static const buttonHeight = TioButtonTokens.height;
+  static const cardPadding = TioCardTokens.padding;
+  static const inputMinHeight = TioInputTokens.minHeight;
+  static const navigationHeight = TioNavigationTokens.bottomBarHeight;
+  static const sheetPadding = TioSheetTokens.padding;
+}
