7d4a8c2f5c9486b246b9f6fcbcf77dc413334ef6
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:32:49 +0530

    feat: add theme foundation palette tokens

---
+++ apps/core/lib/src/theme/tokens/foundation/tio_palette.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TioPalette {
+  const TioPalette._();
+
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+
+  static const neutral50 = Color(0xFFF9FAFB);
+  static const neutral100 = Color(0xFFF3F4F6);
+  static const neutral200 = Color(0xFFE5E7EB);
+  static const neutral300 = Color(0xFFD1D5DB);
+  static const neutral400 = Color(0xFF9CA3AF);
+  static const neutral500 = Color(0xFF6B7280);
+  static const neutral600 = Color(0xFF4B5563);
+  static const neutral700 = Color(0xFF374151);
+  static const neutral800 = Color(0xFF1F2937);
+  static const neutral900 = Color(0xFF111827);
+  static const neutral950 = Color(0xFF0B1120);
+
+  static const red500 = Color(0xFFEF4444);
+  static const green500 = Color(0xFF22C55E);
+  static const violet500 = Color(0xFF8B5CF6);
+  static const cyan500 = Color(0xFF06B6D4);
+  static const amber500 = Color(0xFFF59E0B);
+  static const sky600 = Color(0xFF0284C7);
+}
