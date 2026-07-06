adfc10ecc0cd0cbd13181ae68b317e2845f5de62
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:38:42 +0530

    feat: expand theme local accessors

--- apps/core/lib/src/theme/locals/tio_theme_context.dart
+++ apps/core/lib/src/theme/locals/tio_theme_context.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 
 import '../tio_colors.dart';
+import '../tio_radius.dart';
+import '../tio_shadows.dart';
+import '../tio_spacing.dart';
+import '../tokens/components/tio_button_tokens.dart';
+import '../tokens/components/tio_card_tokens.dart';
+import '../tokens/components/tio_input_tokens.dart';
+import '../tokens/components/tio_navigation_tokens.dart';
+import '../tokens/components/tio_sheet_tokens.dart';
+import '../tokens/domain/tio_domain_colors.dart';
 
 extension TioThemeContext on BuildContext {
   TioColors get tioColors {
     return Theme.of(this).extension<TioColors>() ?? TioColors.light;
   }
+
+  TioShadows get tioShadows {
+    return Theme.of(this).extension<TioShadows>() ?? TioShadows.standard;
+  }
+
+  Type get tioSpacing => TioSpacing;
+  Type get tioRadius => TioRadius;
+  Type get tioButtonTokens => TioButtonTokens;
+  Type get tioCardTokens => TioCardTokens;
+  Type get tioInputTokens => TioInputTokens;
+  Type get tioNavigationTokens => TioNavigationTokens;
+  Type get tioSheetTokens => TioSheetTokens;
+  Type get tioDomainColors => TioDomainColors;
 }
