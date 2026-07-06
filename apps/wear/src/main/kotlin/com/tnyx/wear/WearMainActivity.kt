fdd3c2b5e2d8e7f485b4e3a4d8d1afe75a3e6ab8
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 16:02:18 +0530

    feat(wear): add Wear main activity scaffold

---
+++ apps/wear/src/main/kotlin/com/tnyx/wear/WearMainActivity.kt
@@ -0,0 +1,10 @@
+package com.tnyx.wear
+
+import android.app.Activity
+import android.os.Bundle
+
+class WearMainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
