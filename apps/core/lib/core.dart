b907085814dfc046e0a4f94c20bb48e822f593f6
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 15:31:29 +0530

    feat: expose core components

--- apps/core/lib/core.dart
+++ apps/core/lib/core.dart
@@ -1,7 +1,8 @@
 library tio_core;
 
-export 'src/routing/routing.dart';
-export 'src/ui/shell/presentation/presentation.dart';
 export 'src/theme/locals/theme_locals.dart';
 export 'src/theme/theme.dart';
 export 'src/theme/tokens/tio_tokens.dart';
+export 'src/routing/routing.dart';
+export 'src/ui/components/components.dart';
+export 'src/ui/shell/presentation/presentation.dart';
