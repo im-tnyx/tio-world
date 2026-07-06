d2d07a0487b871afaf0f1410cfa5a9817f06b225
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:28:45 +0530

    add theme provider file

---
+++ apps/core/lib/src/theme/tio_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TioTheme extends StatelessWidget {
+  const TioTheme({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
