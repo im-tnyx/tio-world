b655e349a32218d5444e5782391a22f454c33d94
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 15:05:47 +0530

    refactor: export core ui shell presentation

--- apps/core/lib/core.dart
+++ apps/core/lib/core.dart
@@ -1,6 +1,6 @@
 library tio_core;
 
-export 'src/shell/shell.dart';
+export 'src/ui/shell/presentation/presentation.dart';
 export 'src/theme/locals/theme_locals.dart';
 export 'src/theme/theme.dart';
 export 'src/theme/tokens/tio_tokens.dart';
