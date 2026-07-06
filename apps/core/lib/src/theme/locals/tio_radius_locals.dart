e70a293d8511e1cf0153777c55f2eaa7fff9ba59
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:54:05 +0530

    feat: fill radius locals

--- apps/core/lib/src/theme/locals/tio_radius_locals.dart
+++ apps/core/lib/src/theme/locals/tio_radius_locals.dart
@@ -1,1 +1,10 @@
-// Radius local accessors.
+import '../tokens/foundation/tio_radius.dart';
+
+class TioRadiusLocals {
+  const TioRadiusLocals._();
+
+  static const small = TioRadius.small;
+  static const medium = TioRadius.medium;
+  static const large = TioRadius.large;
+  static const extraLarge = TioRadius.extraLarge;
+}
