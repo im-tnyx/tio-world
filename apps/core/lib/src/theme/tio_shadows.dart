d11231ba0c2ad485b53133a3dc2e23c4f4693c62
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:29:42 +0530

    fill shadow tokens

--- apps/core/lib/src/theme/tio_shadows.dart
+++ apps/core/lib/src/theme/tio_shadows.dart
@@ -1,1 +1,13 @@
-// TODO: add shadow tokens.
+import 'package:flutter/material.dart';
+
+class TioShadows {
+  const TioShadows._();
+
+  static const soft = [
+    BoxShadow(
+      blurRadius: 24,
+      offset: Offset(0, 12),
+      color: Color(0x1A000000),
+    ),
+  ];
+}
