a8ba70419bcd35de1cf4874acf5c73725c997fbe
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:54:33 +0530

    feat: fill shadows locals

--- apps/core/lib/src/theme/locals/tio_shadows_locals.dart
+++ apps/core/lib/src/theme/locals/tio_shadows_locals.dart
@@ -1,1 +1,11 @@
-// Shadows local accessors.
+import 'package:flutter/material.dart';
+
+import '../tokens/effects/tio_shadows.dart';
+
+class TioShadowsLocals {
+  const TioShadowsLocals._();
+
+  static TioShadows of(BuildContext context) {
+    return Theme.of(context).extension<TioShadows>() ?? TioShadows.standard;
+  }
+}
