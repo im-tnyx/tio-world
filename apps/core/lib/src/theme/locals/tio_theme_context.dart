360d11dd9f62aafc4d5226f0561134b315ea350a
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:39:58 +0530

    fix theme local getters

--- apps/core/lib/src/theme/locals/tio_theme_context.dart
+++ apps/core/lib/src/theme/locals/tio_theme_context.dart
@@ -4,12 +4,6 @@ import '../tio_colors.dart';
 import '../tio_radius.dart';
 import '../tio_shadows.dart';
 import '../tio_spacing.dart';
-import '../tokens/components/tio_button_tokens.dart';
-import '../tokens/components/tio_card_tokens.dart';
-import '../tokens/components/tio_input_tokens.dart';
-import '../tokens/components/tio_navigation_tokens.dart';
-import '../tokens/components/tio_sheet_tokens.dart';
-import '../tokens/domain/tio_domain_colors.dart';
 
 extension TioThemeContext on BuildContext {
   TioColors get tioColors {
@@ -20,12 +14,6 @@ extension TioThemeContext on BuildContext {
     return Theme.of(this).extension<TioShadows>() ?? TioShadows.standard;
   }
 
-  Type get tioSpacing => TioSpacing;
-  Type get tioRadius => TioRadius;
-  Type get tioButtonTokens => TioButtonTokens;
-  Type get tioCardTokens => TioCardTokens;
-  Type get tioInputTokens => TioInputTokens;
-  Type get tioNavigationTokens => TioNavigationTokens;
-  Type get tioSheetTokens => TioSheetTokens;
-  Type get tioDomainColors => TioDomainColors;
+  TioSpacing get tioSpacing => TioSpacing.standard;
+  TioRadius get tioRadius => TioRadius.standard;
 }
