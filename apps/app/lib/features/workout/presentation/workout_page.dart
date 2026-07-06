9314f88bf01325631e2d95978c6ce117607d9850
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 12:08:55 +0530

    refactor: migrate workout page

---
+++ apps/app/lib/features/workout/presentation/workout_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../../shared/widgets/placeholder_page.dart';
+
+class WorkoutPage extends StatelessWidget {
+  const WorkoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const PlaceholderPage(
+      title: 'Workout',
+      description: 'Track your exercises and follow workout plans.',
+    );
+  }
+}
