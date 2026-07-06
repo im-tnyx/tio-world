e6412d4090544477d4918ef81ac70b92a807bac3
Santosh Jangid <tnyxapp@gmail.com>
2026-07-06 11:34:11 +0530

    feat: add effect shadow tokens

---
+++ apps/core/lib/src/theme/tokens/effects/tio_shadow_tokens.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TioShadowTokens {
+  const TioShadowTokens._();
+
+  static const soft = [
+    BoxShadow(
+      blurRadius: 24,
+      offset: Offset(0, 12),
+      color: Color(0x1A000000),
+    ),
+  ];
+}
