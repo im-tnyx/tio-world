10ba210adfd45a83226f50ff6800fff008b5be59
Santosh Jangid <tnyxapp@gmail.com>
2026-07-07 02:30:38 +0530

    feat(wear): add Flutter Wear entrypoint

---
+++ apps/wear/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+import 'wear_app.dart';
+
+void main() {
+  runApp(const ProviderScope(child: TioWearApp()));
+}
