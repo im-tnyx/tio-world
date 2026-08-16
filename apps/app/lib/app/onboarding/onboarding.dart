9d756557b5adbccbd3eb41bdd6ae8ff26916d70f
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-16 22:12:49 +0530

    feat(app): export onboarding completion provider

--- apps/app/lib/app/onboarding/onboarding.dart
+++ apps/app/lib/app/onboarding/onboarding.dart
@@ -1,3 +1,4 @@
+export 'onboarding_completion_providers.dart';
 export 'onboarding_status_controller.dart';
 export 'onboarding_status_providers.dart';
 export 'shared_preferences_onboarding_status_repository.dart';
