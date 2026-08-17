7c8302982c9ef692eeebb6f81b2fa135eabf8715
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-17 20:09:20 +0530

    refactor(onboarding): export data collection sheet

--- apps/features/onboarding/lib/src/presentation/widgets/widgets.dart
+++ apps/features/onboarding/lib/src/presentation/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'onboarding_bottom_bar.dart';
 export 'onboarding_content_host.dart';
+export 'onboarding_data_collection_sheet.dart';
 export 'onboarding_progress_indicator.dart';
 export 'onboarding_top_bar.dart';
