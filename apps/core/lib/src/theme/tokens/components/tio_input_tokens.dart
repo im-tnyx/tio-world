6ef39b67c173d5cb456f013ae8691767d43ea309
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-18 21:17:28 +0530

    refactor(theme): alias input typography to canonical tokens

--- apps/core/lib/src/theme/tokens/components/tio_input_tokens.dart
+++ apps/core/lib/src/theme/tokens/components/tio_input_tokens.dart
@@ -1,6 +1,8 @@
 import '../foundation/tio_spacing.dart';
 import '../primitive/tio_alpha.dart';
 import '../primitive/tio_opacity.dart';
+import '../typography/tio_font_size.dart';
+import '../typography/tio_letter_spacing.dart';
 
 class TioInputTokens {
   const TioInputTokens._();
@@ -12,20 +14,20 @@ class TioInputTokens {
   static const focusedOutlineWidth = 1.25;
   static const darkUnfocusedOutlineOpacity = TioOpacity.opacity35;
   static const lightUnfocusedOutlineOpacity = TioOpacity.opacity45;
-  static const compactTextFontSize = 16.0;
-  static const labelFontSize = 14.0;
-  static const compactHintFontSize = 15.0;
-  static const standardHintFontSize = 14.0;
+  static const compactTextFontSize = TioFontSize.size16;
+  static const labelFontSize = TioFontSize.size14;
+  static const compactHintFontSize = TioFontSize.size15;
+  static const standardHintFontSize = TioFontSize.size14;
   static const compactContentVerticalPadding = 10.0;
   static const compactContentHorizontalPadding = TioSpacing.small;
   static const standardContentVerticalPadding = TioSpacing.large;
 
   static const mobileCountryToFieldGap = 14.0;
   static const mobileFieldHeight = 56.0;
-  static const mobileCountryFlagFontSize = 22.0;
-  static const mobileCountryCodeFontSize = 16.0;
-  static const mobileTextFontSize = 16.0;
-  static const mobileTextLetterSpacing = 0.5;
+  static const mobileCountryFlagFontSize = TioFontSize.size22;
+  static const mobileCountryCodeFontSize = TioFontSize.size16;
+  static const mobileTextFontSize = TioFontSize.size16;
+  static const mobileTextLetterSpacing = TioLetterSpacing.positive05;
   static const mobileVerifiedOutlineOpacity = TioOpacity.opacity45;
   static const mobileDefaultOutlineOpacity = TioOpacity.opacity16;
   static const mobileVerifiedOutlineWidth = 1.5;
@@ -34,7 +36,7 @@ class TioInputTokens {
   static const mobileVerifyHorizontalPadding = 10.0;
   static const mobileVerifyVerticalPadding = 5.0;
   static const mobileVerifyContainerOpacity = TioOpacity.opacity09;
-  static const mobileVerifyLabelFontSize = 12.0;
+  static const mobileVerifyLabelFontSize = TioFontSize.size12;
 
   static const usernameIconSize = 20.0;
   static const usernameCheckingIndicatorSize = 16.0;
@@ -44,9 +46,9 @@ class TioInputTokens {
   static const usernameOutlineOpacity = TioOpacity.opacity40;
   static const usernameFocusedOutlineWidth = 2.0;
   static const usernameSupportingGap = 6.0;
-  static const usernameFeedbackFontSize = 12.0;
+  static const usernameFeedbackFontSize = TioFontSize.size12;
   static const usernameSuggestionRadius = 20.0;
   static const usernameSuggestionVerticalPadding = 6.0;
   static const usernameSuggestionOutlineAlpha = TioAlpha.alpha80;
-  static const usernameSuggestionFontSize = 13.0;
+  static const usernameSuggestionFontSize = TioFontSize.size13;
 }
