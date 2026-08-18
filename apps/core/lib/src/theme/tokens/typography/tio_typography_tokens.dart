42bb1c1ecbe1edb498f1448392f25afadb79cb7b
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-18 21:00:54 +0530

    refactor(theme): export canonical typography primitives

--- apps/core/lib/src/theme/tokens/typography/tio_typography_tokens.dart
+++ apps/core/lib/src/theme/tokens/typography/tio_typography_tokens.dart
@@ -1,1 +1,6 @@
+export 'tio_font_family.dart';
+export 'tio_font_size.dart';
+export 'tio_font_weight.dart';
+export 'tio_letter_spacing.dart';
+export 'tio_line_height.dart';
 export 'tio_typography.dart';
