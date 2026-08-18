96c3fc4f09fcff715c268df47fa6d8fea235d571
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-18 21:41:44 +0530

    refactor(theme): alias button strokes to TioStroke

--- apps/core/lib/src/theme/tokens/components/tio_button_tokens.dart
+++ apps/core/lib/src/theme/tokens/components/tio_button_tokens.dart
@@ -1,5 +1,6 @@
 import '../foundation/tio_radius.dart';
 import '../foundation/tio_spacing.dart';
+import '../foundation/tio_stroke.dart';
 import '../primitive/tio_opacity.dart';
 
 class TioButtonTokens {
@@ -11,12 +12,12 @@ class TioButtonTokens {
   static const horizontalPadding = 20.0;
   static const contentGap = TioSpacing.small;
   static const loadingIndicatorSize = 18.0;
-  static const loadingIndicatorStrokeWidth = 2.0;
+  static const loadingIndicatorStrokeWidth = TioStroke.width2;
   static const pressedStateOpacity = TioOpacity.opacity12;
   static const focusedStateOpacity = TioOpacity.opacity12;
   static const hoveredStateOpacity = TioOpacity.opacity08;
   static const disabledContainerOpacity = TioOpacity.opacity12;
   static const disabledContentOpacity = TioOpacity.opacity38;
-  static const outlineWidth = 1.0;
-  static const focusedOutlineWidth = 2.0;
+  static const outlineWidth = TioStroke.width1;
+  static const focusedOutlineWidth = TioStroke.width2;
 }
