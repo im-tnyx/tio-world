84b5b6f5bd2024e5126bcc3a48887478b1d9d72b
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-18 21:19:27 +0530

    refactor(theme): alias remove-image typography

--- apps/core/lib/src/theme/tokens/components/tio_remove_image_sheet_tokens.dart
+++ apps/core/lib/src/theme/tokens/components/tio_remove_image_sheet_tokens.dart
@@ -1,6 +1,8 @@
 import '../foundation/tio_radius.dart';
 import '../foundation/tio_spacing.dart';
 import '../primitive/tio_alpha.dart';
+import '../typography/tio_font_size.dart';
+import '../typography/tio_letter_spacing.dart';
 
 class TioRemoveImageSheetTokens {
   const TioRemoveImageSheetTokens._();
@@ -12,16 +14,16 @@ class TioRemoveImageSheetTokens {
   static const closeButtonSize = 32.0;
   static const closeIconSize = 18.0;
   static const closeToTitleGap = 6.0;
-  static const titleFontSize = 22.0;
-  static const titleLetterSpacing = -0.3;
+  static const titleFontSize = TioFontSize.size22;
+  static const titleLetterSpacing = TioLetterSpacing.negative03;
   static const titleToSubtitleGap = TioSpacing.small;
-  static const subtitleFontSize = 15.0;
+  static const subtitleFontSize = TioFontSize.size15;
   static const subtitleToActionsGap = 26.0;
   static const actionRadius = 20.0;
   static const actionVerticalPadding = TioSpacing.large;
   static const actionOutlineAlpha = TioAlpha.alpha25;
   static const actionOutlineWidth = 1.0;
-  static const actionLabelFontSize = 16.0;
+  static const actionLabelFontSize = TioFontSize.size16;
   static const actionIconGap = TioSpacing.small;
   static const removeIconSize = 20.0;
   static const cancelIconSize = 18.0;
