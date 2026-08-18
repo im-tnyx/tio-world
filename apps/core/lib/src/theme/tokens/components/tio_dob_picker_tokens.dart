b9c286e3a3e0c1aaccfbe6ec03b61272d99fb2a7
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-18 21:18:08 +0530

    refactor(theme): alias DOB typography to canonical tokens

--- apps/core/lib/src/theme/tokens/components/tio_dob_picker_tokens.dart
+++ apps/core/lib/src/theme/tokens/components/tio_dob_picker_tokens.dart
@@ -1,18 +1,20 @@
 import '../foundation/tio_spacing.dart';
 import '../primitive/tio_alpha.dart';
+import '../typography/tio_font_size.dart';
+import '../typography/tio_letter_spacing.dart';
 import 'tio_wheel_picker_tokens.dart';
 
 class TioDobPickerTokens {
   const TioDobPickerTokens._();
 
   static const sheetOutlineAlpha = TioAlpha.alpha25;
-  static const titleFontSize = 22.0;
-  static const titleLetterSpacing = -0.2;
+  static const titleFontSize = TioFontSize.size22;
+  static const titleLetterSpacing = TioLetterSpacing.negative02;
   static const closeIconSize = 24.0;
   static const closeSplashRadius = 20.0;
   static const headerSubtitleGap = 6.0;
-  static const subtitleFontSize = 14.0;
-  static const columnHeaderFontSize = 17.0;
+  static const subtitleFontSize = TioFontSize.size14;
+  static const columnHeaderFontSize = TioFontSize.size17;
   static const columnHeaderToWheelGap = TioSpacing.medium;
   static const wheelHeight = TioWheelPickerTokens.viewportHeight;
   static const selectionHeight = TioWheelPickerTokens.selectionHeight;
@@ -23,6 +25,6 @@ class TioDobPickerTokens {
   static const perspective = 0.004;
   static const diameterRatio = 1.3;
   static const selectedFontSize = TioWheelPickerTokens.selectedFontSize;
-  static const unselectedFontSize = 17.0;
+  static const unselectedFontSize = TioFontSize.size17;
   static const unselectedTextAlpha = TioAlpha.alpha120;
 }
