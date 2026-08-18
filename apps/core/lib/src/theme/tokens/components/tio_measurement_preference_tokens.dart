838e36e23fea8baf3dcd11e7485d971fddac574e
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-18 23:46:52 +0530

    refactor(core): delete measurement preference token proxy

--- apps/core/lib/src/theme/tokens/components/tio_measurement_preference_tokens.dart
+++
@@ -1,7 +0,0 @@
-import '../typography/tio_font_size.dart';
-
-class TioMeasurementPreferenceTokens {
-  const TioMeasurementPreferenceTokens._();
-
-  static const sectionLabelFontSize = TioFontSize.size13;
-}
