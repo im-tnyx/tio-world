684654490858e5f17a206a796d8a8d1d6cef2183
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-19 08:26:03 +0530

    refactor(theme): add auth evidenced letter spacing

--- apps/core/lib/src/theme/tokens/typography/tio_letter_spacing.dart
+++ apps/core/lib/src/theme/tokens/typography/tio_letter_spacing.dart
@@ -4,5 +4,6 @@ abstract final class TioLetterSpacing {
   static const negative02 = -0.2;
   static const positive05 = 0.5;
   static const positive08 = 0.8;
+  static const positive10 = 1.0;
   static const positive60 = 6.0;
 }
