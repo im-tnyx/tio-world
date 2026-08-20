90abca480d7d9ddc460760a3fd158731da4a807d
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-20 09:29:16 +0530

    feat(profile): export Profile Settings repository

--- apps/features/profile/lib/src/domain/repositories/repositories.dart
+++ apps/features/profile/lib/src/domain/repositories/repositories.dart
@@ -1,4 +1,5 @@
 export 'account_setup_repository.dart';
 export 'measurement_unit_preferences_repository.dart';
 export 'profile_account_repository.dart';
+export 'profile_settings_repository.dart';
 export 'profile_setup_repository.dart';
