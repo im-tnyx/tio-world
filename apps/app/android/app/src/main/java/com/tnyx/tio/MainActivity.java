dbe8fbe064921a6e47eb8e8ff2ea2ad36afab61f
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-23 06:01:25 +0530

    refactor(app): keep Health Connect bridge dependency-free

--- apps/app/android/app/src/main/java/com/tnyx/tio/MainActivity.java
+++ apps/app/android/app/src/main/java/com/tnyx/tio/MainActivity.java
@@ -1,13 +1,11 @@
 package com.tnyx.tio;
 
-import androidx.annotation.NonNull;
-
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
     @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
         HealthConnectAvailabilityBridge.register(
                 this,
