6764fb51a0aadc45dfeaac44efd3199d391a02c5
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-24 22:00:12 +0530

    refactor(startup): keep bootstrap runApp-only

--- apps/app/lib/app/bootstrap.dart
+++ apps/app/lib/app/bootstrap.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 void bootstrap(Widget Function() builder) {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(builder());
 }
