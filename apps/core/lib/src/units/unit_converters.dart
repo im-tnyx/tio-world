7ff051d6ea1ce391e270901c08a366cff7ac2964
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-08-27 10:07:55 +0530

    refactor(core): remove converter compatibility bridge

--- apps/core/lib/src/units/unit_converters.dart
+++ apps/core/lib/src/units/unit_converters.dart
@@ -29,38 +29,3 @@ class UnitConverters {
   static double flOzToMl(double fluidOunces) =>
       fluidOunces / usFluidOuncesPerMillilitre;
 }
-
-/// Temporary migration bridge while repository-wide consumers move to
-/// [UnitConverters]. Issue #23 requires removal before final acceptance.
-@Deprecated('Use UnitConverters')
-class MeasurementConverters {
-  const MeasurementConverters._();
-
-  static const double poundsPerKilogram = UnitConverters.poundsPerKilogram;
-  static const double centimetresPerInch = UnitConverters.centimetresPerInch;
-  static const double milesPerKilometre = UnitConverters.milesPerKilometre;
-  static const double usFluidOuncesPerMillilitre =
-      UnitConverters.usFluidOuncesPerMillilitre;
-
-  static double kgToLb(double kilograms) => UnitConverters.kgToLb(kilograms);
-  static double lbToKg(double pounds) => UnitConverters.lbToKg(pounds);
-
-  static double cmToTotalInches(double centimetres) =>
-      UnitConverters.cmToTotalInches(centimetres);
-  static double totalInchesToCm(double inches) =>
-      UnitConverters.totalInchesToCm(inches);
-
-  static ({int feet, int inches}) cmToFeetInches(double centimetres) =>
-      UnitConverters.cmToFeetInches(centimetres);
-
-  static double feetInchesToCm({required int feet, required int inches}) =>
-      UnitConverters.feetInchesToCm(feet: feet, inches: inches);
-
-  static double kmToMi(double kilometres) => UnitConverters.kmToMi(kilometres);
-  static double miToKm(double miles) => UnitConverters.miToKm(miles);
-
-  static double mlToFlOz(double millilitres) =>
-      UnitConverters.mlToFlOz(millilitres);
-  static double flOzToMl(double fluidOunces) =>
-      UnitConverters.flOzToMl(fluidOunces);
-}
