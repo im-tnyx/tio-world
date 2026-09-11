96b197aa9e8aedf8458101c6012b186ca67c2158
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-09-11 22:57:51 +0530

    refactor(nutrition): export Meal Diary preference providers

--- apps/features/nutrition/lib/src/meal_diary/meal_diary.dart
+++ apps/features/nutrition/lib/src/meal_diary/meal_diary.dart
@@ -1,4 +1,5 @@
 export 'data/shared_preferences_meal_diary_display_preferences_repository.dart';
 export 'domain/models/meal_diary_display_preferences.dart';
 export 'domain/repositories/meal_diary_display_preferences_repository.dart';
+export 'meal_diary_display_preferences_providers.dart';
 export 'presentation/presentation.dart';
