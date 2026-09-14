41e38f1440005c1afa8f8da1a9dc4dfe0e892162
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-09-14 10:44:04 +0530

    test(nutrition): import provider scope for section coverage

--- apps/features/nutrition/test/meal_diary/meal_diary_section_incomplete_protein_test.dart
+++ apps/features/nutrition/test/meal_diary/meal_diary_section_incomplete_protein_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tio_core/core.dart';
 import 'package:tio_feature_nutrition/nutrition.dart';
